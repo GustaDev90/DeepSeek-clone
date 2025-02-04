1c48b26f546714af70ef04bd4235d4a9cbe75b3d
GustaDev <gustagames0912@gmail.com>
2025-02-04 14:21:16 -0300

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFound: React.FC = () => {
     <div className="flex h-screen flex-col items-center justify-center -mt-16">
       <Image src="/deepseeknot.png" width={420} height={100} alt="Not Found" />
       <div className="flex flex-col gap-3 text-center">
-        <p className="md:ml-14">Our website is currently under maintenance.</p>
+        <p className="max-w-md mx-auto">Our website is currently under maintenance.</p>
         <p>We apologize for the inconvenience, we will be back shortly.</p>
       </div>
     </div>
